880b9da121a93ed7763294d204cba1590317b0b7
Sarah Vo <srvo9803@gmail.com>
2022-10-08 19:59:37 -0700

    Changed Rule to virtual function

--- web-socket-networking/lib/gamestuff/include/Rules.h
+++ web-socket-networking/lib/gamestuff/include/Rules.h
@@ -1,27 +1,7 @@
 #include<vector>
 #include<string>
-//General rule struct to store various different rule elements 
-//(will need to change this to each ruletype)
-struct RuleType{
-    std::string rulesType;
-    //Some rule types use list
-    std::vector<std::string> list;
-    //Some rules use value
-    auto value;
-    //Many rule execute the rules list on list elements 
-    //Not certain how to operate with This
-    std::vector<std::string> rulesList;
-}
-template <typename T>
+
 class Rules{
     public:
-        Rules(void) {};
-        ~Rules(void) {};
-
-        std::string ruleSetOperation(std::string rule, T info);
-        //A couple of rule setters
-        void setRuleOnList(RulesType ruleT, std::string rule);
-        void setTimerRule(RulesType ruleT,int duration, std::string mode )
-        void setMessageRule(RulesType ruleT,std::string message);
-        void setGlobalMessageRule(RulesType ruleT,std::string message);
+        virtual void runRule() = 0;
 }
