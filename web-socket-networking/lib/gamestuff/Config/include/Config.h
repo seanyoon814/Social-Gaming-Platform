821e65bbb7c5d5ec3ea722c60b1c5eb556570595
shy13 <shy13@sfu.ca>
2022-10-16 19:04:42 -0700

    commit to save structure for unit tests

---
+++ web-socket-networking/lib/gamestuff/Config/include/Config.h
@@ -0,0 +1,63 @@
+#include <vector>
+#include <string>
+#include <map>
+#include <cassert>
+#include <algorithm>
+#include <stdlib.h>
+#include <jsoncpp/json/value.h>
+#include <jsoncpp/json/json.h>
+
+#define defaultRounds 10;
+
+namespace std{
+
+    struct GameSetup{
+        //number of rounds
+        int rounds = defaultRounds;
+    };
+
+    class Config{
+    public:
+        //Config constructor
+        Config(map<string, Json::Value> json_values);
+        //Destructor
+        ~Config();
+        //Create the game setup. Not creating game setup will result in default rounds of 10.
+        void createGameSetup(GameSetup gameSetup, int numRounds);
+        //Change the min number of players
+        void changeMin(int newMin) noexcept;
+        //Change the max number of players
+        void changeMax(int newMax) noexcept;
+        //change the name of the game
+        void changeName(string newName) noexcept;
+        void changeAudience(bool newAudience) noexcept;
+        void setJSON(map<string, Json::Value> json_values);
+        map<string, string> getJSON() const;
+        //return game setup struct
+        GameSetup getSetup()noexcept;
+        //Assert the things in the json
+        bool assertJSON() const;
+        //set values from JSON file into Config class
+        void setVariables();
+        //set setup bool variable
+        void setSetup(bool n);
+    private:
+        //Name of the game
+        map<string, Json::Value> json;
+        //
+        vector<string> attributes = {"name", "player count", "audience", "setup", "min", "max", "audience", "setup"};
+
+        string name;
+        //Minimum number of players
+        int min;
+        //Maximum number of players
+        int max;
+        //If an audience is allowed
+        bool audience;
+        //Setup struct that contains rounds, quiz questions + answers, etc.
+        //Will need to adjust this struct later according to type of game in a later iteration
+        GameSetup setup;
+        
+        bool hasSetup;
+};
+}
