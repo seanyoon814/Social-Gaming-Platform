2c6ba283fd389978e2fc96aae4b9c1ef4a99d853
dca119 <dca119@sfu.ca>
2022-11-17 00:14:46 -0800

    some functions added

--- web-socket-networking/lib/gamestuff/Data/DataType.cpp
+++ web-socket-networking/lib/gamestuff/Data/DataType.cpp
@@ -1,6 +1,43 @@
 #include "DataType.h"
 
-void DataTypes::addToDataMap(std::string type, std::map<Data, Data> map)
+void DataTypes::addToDataMap(std::string type, std::map<std::string, std::vector<std::map<Data, Data>>> map)
 {
     dataMap.insert(std::make_pair(type, map));
+};
+
+bool DataTypes::contains(std::map<Data, Data> map, std::string string)
+{
+    std::map<Data, Data>::iterator it;
+    it = map.find(StringObj(string));
+    if (it != map.end())
+    {
+        return true;
+    }
+    return false;
+};
+
+int DataTypes::getSize(std::map<Data, Data> map)
+{
+    return map.size();
+};
+
+// need to make parameter string structs
+std::vector<std::map<Data, Data>> DataTypes::elements(std::string type, std::string toSearch)
+{
+    std::vector<std::map<Data, Data>> elements;
+    std::map<std::string, std::map<std::string, std::vector<std::map<Data, Data>>>>::iterator it;
+    it = dataMap.find(type);
+    if (it == dataMap.end())
+    {
+        return elements;
+    }
+    std::map<std::string, std::vector<std::map<Data, Data>>> typeMap = it->second;
+    std::map<std::string, std::vector<std::map<Data, Data>>>::iterator iter;
+    iter = typeMap.find(toSearch);
+    if (iter == typeMap.end())
+    {
+        return elements;
+    }
+    elements = iter->second;
+    return elements;
 };
--- web-socket-networking/lib/gamestuff/Data/include/DataType.h
+++ web-socket-networking/lib/gamestuff/Data/include/DataType.h
@@ -1,16 +1,25 @@
 #include "Data.h"
 #include <vector>
 
+struct condition{
+    std::string cond1;
+    std::string cond2;
+};
+
+// maybe change to map to string struct for parameters
+
 class DataTypes
 {
 public:
-    void addToDataMap(std::string, std::map<Data, Data>);
-    // std::vector elements(std::vector toSearch);
-    // bool contains(std::string);
+    void addToDataMap(std::string, std::map<std::string, std::vector<std::map<Data, Data>>>);
+    std::vector<std::map<Data, Data>> elements(std::string, std::string);
+    bool contains(std::map<Data, Data>, std::string);
+    int getSize(std::map<Data, Data>);
+    std::vector<std::map<Data, Data>> collect(condition);
 
 private:
     // Main data map (type -> their map)
-    std::map<std::string, std::map<Data, Data>> dataMap;
+    std::map<std::string, std::map<std::string, std::vector<std::map<Data, Data>>>> dataMap;
 
     // Each Data type layout
     // std::map<std::string, std::vector<std::map<Data, Data>>> constants;
